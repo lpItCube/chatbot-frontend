f62893dcc8ab5026924e898ae8a4aecfe2c52a8e
pabol <pgm118@alu.ua.es>
2022-05-20 12:52:14 +0200

    Test module

---
+++ src/app/modules/formcomponent/formcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formcomponent',
+  templateUrl: './formcomponent.component.html',
+  styleUrls: ['./formcomponent.component.css']
+})
+export class FormcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

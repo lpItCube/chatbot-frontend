e52eae44e5c39d115970486b30d830f455faa1f8
AitorUA <ama227@alu.ua.es>
2022-06-10 12:30:45 +0200

    uwu

--- src/app/modules/preview/preview.component.ts
+++ src/app/modules/preview/preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ViewEncapsulation } from '@angular/core';
+import { NodeService } from 'src/app/services/node.service';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
@@ -11,15 +12,46 @@ import { ViewEncapsulation } from '@angular/core';
 export class PreviewComponent implements OnInit {
 
   public id = "";
+  public nodesOfCollection = [];
+  public currentNode = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private nodeservice: NodeService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') as any;
     console.log(this.id);
+    this.currentNode = 0;
+    this.getNodes();
   }
 
-  
+  getNodes(){
+    console.log(this.id);
+    this.nodeservice.getNodesCollID(this.id).subscribe( async (res:any) => {
+      console.log(res);
+      this.nodesOfCollection = res;
+      this.createQuestion();
+      /*this.allOptions = [];
+      for(let i=0;i<this.nodeNumber;i++){
+        for(let j=0;j<res[i]['options'].length;j++){
+          this.allOptions.push(res[i]['options'][j]);
+        } 
+      }
+      console.log(this.allOptions);
+      this.linksOptions = await this.optionComponent.getLinkOptions(this.allOptions);
+      console.log(this.linksOptions);*/
+    });
+  }
+
+  createQuestion(){
+    console.log(this.nodesOfCollection[this.currentNode].description);
+    if(this.nodesOfCollection[this.currentNode].type == "Normale"){
+
+    }else if(this.nodesOfCollection[this.currentNode].type == "input"){
+
+    }
+    this.currentNode++;
+  }
 
   createAnswer(text){
 

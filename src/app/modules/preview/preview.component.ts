a074352fafb2b01a53a669445f5ac2b7acd20ec2
pabol <pgm118@alu.ua.es>
2022-06-10 11:53:26 +0200

    User answer done

--- src/app/modules/preview/preview.component.ts
+++ src/app/modules/preview/preview.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.css']
+  styleUrls: ['./preview.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class PreviewComponent implements OnInit {
 
   public id = "";
@@ -15,6 +17,33 @@ export class PreviewComponent implements OnInit {
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') as any;
     console.log(this.id);
+  }
+
+  
+
+  createAnswer(text){
+
+    var container = document.getElementById("container");
+
+    var answerDiv = document.createElement("div");
+        answerDiv.setAttribute('class', 'rightMessage');
+
+    var answer = document.createElement("div");
+        answer.setAttribute('class', 'message');
+
+    var p = document.createElement("p")
+    var texto = document.createTextNode(text);
+
+    p.appendChild(texto);
+    answer.appendChild(p);
+    answerDiv.appendChild(answer);
+    container.appendChild(answerDiv);
+
+
+    var options = document.getElementById("options");
+
+    options.parentNode.removeChild(options);
+
   }
 
 }
